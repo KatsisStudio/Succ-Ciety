7485a7fe04c50a1a9a7fc4bfb51a53172af17b40
Xwilarg <xwilarg@protonmail.com>
2023-11-05 23:53:48 +0100

    Fix UI display

---
+++ Assets/Scripts/Lobby/StatDisplay.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LewdieJam.Lobby
+{
+    public class StatDisplay : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _value;
+
+        [SerializeField]
+        private Button _add;
+    }
+}
