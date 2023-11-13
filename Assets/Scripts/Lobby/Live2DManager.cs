e4bfdcd1061dd791e5f1c11bb7a9f0cc79e06d92
Xwilarg <xwilarg@protonmail.com>
2023-11-13 18:00:18 +0100

    Move live2D management in prefab

---
+++ Assets/Scripts/Lobby/Live2DManager.cs
@@ -0,0 +1,131 @@
+﻿using LewdieJam.Game;
+using LewdieJam.SO;
+using Live2D.Cubism.Core;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace LewdieJam.Lobby
+{
+    public class Live2DManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameInfo _gameInfo;
+
+        [SerializeField]
+        private CubismParameter _breathParam;
+        [SerializeField]
+        private CubismParameter _breastsBounceParam;
+        [SerializeField]
+        private CubismParameter _breastsSizeModifierParam;
+        [SerializeField]
+        private CubismParameter _hornParam;
+
+        private readonly List<UpdateTimer> _timers = new();
+
+        private float _hornTimerTarget;
+
+        private bool _live2dDirty;
+
+        private float _hornTimer;
+        private int _hornIndex;
+
+        private void Awake()
+        {
+            _timers.Add(new(_breathParam, .5f, true));
+        }
+
+        private void Update()
+        {
+            foreach (var timer in _timers)
+            {
+                timer.Update(Time.deltaTime);
+            }
+
+            _timers.RemoveAll(x => x.IsDeletionCandidate);
+
+            if (_hornTimer < _hornTimerTarget)
+            {
+                _hornTimer += Time.deltaTime * .1f;
+                if (_hornTimer > _hornTimerTarget)
+                {
+                    _hornTimer = _hornTimerTarget;
+                }
+                var tatooDelta = _hornParam.MaximumValue - _hornParam.MinimumValue;
+                _hornParam.Value = _hornTimer * _hornParam.MaximumValue + _hornParam.MinimumValue;
+            }
+
+            // Somehow this doesn't work properly if done in UpdateUI (?)
+            if (_live2dDirty)
+            {
+                _live2dDirty = false;
+                Debug.Log("Update breasts size");
+                _breastsSizeModifierParam.Value = PersistentData.Attachments.HasFlag(Attachment.LargeBreasts) ? _breastsSizeModifierParam.MaximumValue : _breastsSizeModifierParam.MinimumValue;
+                Debug.Log(_breastsSizeModifierParam.Value);
+            }
+        }
+
+        public void SetHornLevel(int index)
+        {
+            _hornIndex = index;
+            _hornTimerTarget = _gameInfo.HornLevels[_hornIndex];
+            _hornParam.Value = _hornTimerTarget * _hornParam.MaximumValue + _hornParam.MinimumValue;
+        }
+
+        public void OnBreastsClick()
+        {
+            if (!_timers.Any(x => x.IsId(_breastsBounceParam.Id)))
+            {
+                _timers.Add(new(_breastsBounceParam, 1.5f, false));
+            }
+        }
+
+        public void SetDirty()
+        {
+            _live2dDirty = true;
+        }
+
+        class UpdateTimer
+        {
+            public UpdateTimer(CubismParameter cParam, float speedMultiplier, bool isPersistent)
+            {
+                _param = cParam;
+                _speedMultiplier = speedMultiplier;
+                _isPersistent = isPersistent;
+            }
+
+            public void Update(float deltaTime)
+            {
+                _timer += deltaTime * (_isGoingUp ? 1f : -1f) * _speedMultiplier;
+
+                if (_isGoingUp && _timer >= _param.MaximumValue)
+                {
+                    _timer = _param.MaximumValue;
+                    _isGoingUp = false;
+                }
+                else if (!_isGoingUp && _timer <= _param.MinimumValue)
+                {
+                    _timer = _param.MinimumValue;
+                    _isGoingUp = true;
+                    if (!_isPersistent)
+                    {
+                        _isDeletionCandidate = true;
+                    }
+                }
+
+                _param.Value = _timer;
+            }
+
+            public bool IsDeletionCandidate => _isDeletionCandidate;
+
+            public bool IsId(string id) => _param.Id == id;
+
+            private float _timer;
+            private float _speedMultiplier;
+            private bool _isGoingUp = true;
+            private CubismParameter _param;
+            private bool _isPersistent;
+            private bool _isDeletionCandidate;
+        }
+    }
+}
--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -1,8 +1,6 @@
 ﻿using LewdieJam.Game;
 using LewdieJam.SO;
-using Live2D.Cubism.Core;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -15,11 +13,9 @@ namespace LewdieJam.Lobby
     {
         public static LobbyManager Instance { private set; get; }
 
-        [Header("Game Info SO")]
         [SerializeField]
         private GameInfo _gameInfo;
 
-        [Header("UI")]
         [SerializeField]
         private StatDisplay _healthStat, _atkPowerStat, _atkSpeedStat, _energyStat;
 
@@ -34,33 +30,11 @@ namespace LewdieJam.Lobby
 
         [Header("Live2D")]
         [SerializeField]
-        private CubismParameter _breathParam;
-        [SerializeField]
-        private CubismParameter _breastsBounceParam;
-        [SerializeField]
-        private CubismParameter _breastsSizeModifierParam;
-        [SerializeField]
-        private CubismParameter _hornParam;
+        private Live2DManager _live2D;
 
         [Header("Debug")]
         [SerializeField]
         private bool _giveLotOfEnergy;
-
-        private bool _live2dDirty;
-
-        private readonly List<UpdateTimer> _timers = new();
-
-        private float _hornTimer;
-        private float _hornTimerTarget;
-        private int _hornIndex;
-
-        public void OnBreastsClick()
-        {
-            if (!_timers.Any(x => x.IsId(_breastsBounceParam.Id)))
-            {
-                _timers.Add(new(_breastsBounceParam, 1.5f, false));
-            }
-        }
 
         public void LoadGame()
         {
@@ -83,43 +57,8 @@ namespace LewdieJam.Lobby
             _atkSpeedStat.Key = UpgradableStat.CharmPower;
             _energyStat.Key = UpgradableStat.EnergyGained;
 
-            _hornIndex = Mathf.FloorToInt(Stat01 * (_gameInfo.HornLevels.Length - 1));
-
-            var tatooDelta = _hornParam.MaximumValue - _hornParam.MinimumValue;
-            _hornTimerTarget = _gameInfo.HornLevels[_hornIndex];
-            _hornParam.Value = _hornTimerTarget * _hornParam.MaximumValue + _hornParam.MinimumValue;
-
+            _live2D.SetHornLevel(Mathf.FloorToInt(Stat01 * (_gameInfo.HornLevels.Length - 1)));
             UpdateUI();
-
-            _timers.Add(new(_breathParam, .5f, true));
-        }
-
-        private void Update()
-        {
-            foreach (var timer in _timers)
-            {
-                timer.Update(Time.deltaTime);
-            }
-
-            _timers.RemoveAll(x => x.IsDeletionCandidate);
-
-            if (_hornTimer < _hornTimerTarget)
-            {
-                _hornTimer += Time.deltaTime * .1f;
-                if (_hornTimer > _hornTimerTarget)
-                {
-                    _hornTimer = _hornTimerTarget;
-                }
-                var tatooDelta = _hornParam.MaximumValue - _hornParam.MinimumValue;
-                _hornParam.Value = _hornTimer * _hornParam.MaximumValue + _hornParam.MinimumValue;
-            }
-
-            // Somehow this doesn't work properly if done in UpdateUI (?)
-            if (_live2dDirty)
-            {
-                _live2dDirty = false;
-                _breastsSizeModifierParam.Value = PersistentData.Attachments.HasFlag(Attachment.LargeBreasts) ? _breastsSizeModifierParam.MaximumValue : _breastsSizeModifierParam.MinimumValue;
-            }
         }
 
         private void ToggleAttachment(Attachment attachment)
@@ -143,13 +82,13 @@ namespace LewdieJam.Lobby
         public void UpdateUI()
         {
             _energy.text = PersistentData.Energy.ToString();
-            _hornIndex = Mathf.FloorToInt(Stat01 * (_gameInfo.HornLevels.Length - 1));
-            _hornTimerTarget = _gameInfo.HornLevels[_hornIndex];
-            _hornLevel.text = _hornIndex.ToString();
+            var index = Mathf.FloorToInt(Stat01 * (_gameInfo.HornLevels.Length - 1));
+            _live2D.SetHornLevel(index);
+            _hornLevel.text = index.ToString();
 
             for (int i = 0; i < _buttonsPerHornLevels.Length; i++)
             {
-                if (_hornIndex > i * 2)
+                if (index > i * 2)
                 {
                     _buttonsPerHornLevels[i].interactable = true;
                 }
@@ -184,50 +123,7 @@ namespace LewdieJam.Lobby
                 }
             }
 
-            _live2dDirty = true;
+            _live2D.SetDirty();
         }
-    }
-
-    class UpdateTimer
-    {
-        public UpdateTimer(CubismParameter cParam, float speedMultiplier, bool isPersistent)
-        {
-            _param = cParam;
-            _speedMultiplier = speedMultiplier;
-            _isPersistent = isPersistent;
-        }
-
-        public void Update(float deltaTime)
-        {
-            _timer += deltaTime * (_isGoingUp ? 1f : -1f) * _speedMultiplier;
-
-            if (_isGoingUp && _timer >= _param.MaximumValue)
-            {
-                _timer = _param.MaximumValue;
-                _isGoingUp = false;
-            }
-            else if (!_isGoingUp && _timer <= _param.MinimumValue)
-            {
-                _timer = _param.MinimumValue;
-                _isGoingUp = true;
-                if (!_isPersistent)
-                {
-                    _isDeletionCandidate = true;
-                }
-            }
-
-            _param.Value = _timer;
-        }
-
-        public bool IsDeletionCandidate => _isDeletionCandidate;
-
-        public bool IsId(string id) => _param.Id == id;
-
-        private float _timer;
-        private float _speedMultiplier;
-        private bool _isGoingUp = true;
-        private CubismParameter _param;
-        private bool _isPersistent;
-        private bool _isDeletionCandidate;
     }
 }
