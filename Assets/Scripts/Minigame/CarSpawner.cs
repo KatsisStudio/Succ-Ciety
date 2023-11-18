fe9f1bf95e2bc93f22e7ce745c2c80d4880def55
Xwilarg <xwilarg@protonmail.com>
2023-11-18 17:01:39 +0100

    Add minigame

---
+++ Assets/Scripts/Minigame/CarSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace LewdieJam.Minigame
+{
+    public class CarSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private float _min, _max;
+
+        [SerializeField]
+        private GameObject _car;
+
+        [SerializeField]
+        private float _carSpeed;
+
+        [SerializeField]
+        private bool _lookRight;
+
+        private void Awake()
+        {
+            StartCoroutine(Spawn());
+        }
+
+        private IEnumerator Spawn()
+        {
+            while (true)
+            {
+                var car = Instantiate(_car, transform.position, _car.transform.rotation);
+                car.GetComponent<Car>().Speed = _carSpeed;
+                if (!_lookRight)
+                {
+                    car.transform.rotation = Quaternion.Euler(_car.transform.rotation.eulerAngles.x, _car.transform.rotation.eulerAngles.y, _car.transform.rotation.eulerAngles.z + 180f);
+                }
+                Destroy(car, 10f);
+
+                yield return new WaitForSeconds(Random.Range(_min, _max));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Minigame/MPlayerController.cs
@@ -0,0 +1,62 @@
+﻿using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace LewdieJam.Minigame
+{
+    public class MPlayerController : MonoBehaviour
+    {
+        private Rigidbody _rb;
+
+        private float _zDest;
+        private const float _zOffset = 12f;
+        private const float _speed = 10f;
+
+        private bool _isDead;
+
+        [SerializeField]
+        private CinemachineVirtualCamera _cam;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.CompareTag("Car") && !_isDead)
+            {
+                _cam.LookAt = null;
+                _cam.Follow = null;
+                _isDead = true;
+
+                var or = transform.position;
+                var car = collision.collider.transform.position;
+                _rb.AddForce((new Vector3(or.x - car.x, 0, 0).normalized + Vector3.up) * 100f, ForceMode.Impulse);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_isDead)
+            {
+                if (transform.position.z > _zDest)
+                {
+                    _rb.velocity = Vector3.zero;
+                }
+                else
+                {
+                    _rb.velocity = Vector3.forward * _speed;
+                }
+            }
+        }
+
+        public void OnJump(InputAction.CallbackContext value)
+        {
+            if (value.performed && !_isDead)
+            {
+                _zDest += _zOffset;
+            }
+        }
+    }
+}
